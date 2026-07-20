b031fd450a1911003f73438ee504188e1b7dca6b
Nat! <nat@mulle-kybernetik.com>
2026-07-20 17:00:03 +0200

    CI settings, extension freshening, and prerelease setup

--- src/MulleScionHTMLPreprocessor.h
+++ src/MulleScionHTMLPreprocessor.h
@@ -9,7 +9,7 @@
 #import "import.h"
 
 
-#define MULLE_SCION_HTML_PREPROCESSOR_VERSION  ((0UL << 20) | (3 << 8) | 0)
+#define MULLE_SCION_HTML_PREPROCESSOR_VERSION  ((0UL << 20) | (3 << 8) | 1)
 
 
 @interface MulleScionHTMLPreprocessor : NSObject < MulleObjCThreadSafe>
